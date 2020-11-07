a9f127094029981eeee699c109d5605ea46a0df1
bhwang1 <66380996+bhwang1@users.noreply.github.com>
2020-11-06 21:50:37 -0500

    lab 4

---
+++ public/lab_4/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.route('/api')
+  .get((req, res) => {
+    console.log('GET request detected');
+  })
+  .post((req, res) => {
+    console.log('POST request detected');
+    console.log('Form data submitted', req.body);
+    res.status(200).send('Hello World');
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
