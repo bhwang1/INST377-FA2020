b51b91cdaaa8dcdba2b10e5d48c0dd41f6ee9b15
bhwang1 <66380996+bhwang1@users.noreply.github.com>
2020-11-07 16:00:21 -0500

    function updates

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,8 @@
-// You may wish to find an effective randomizer function on MDN.
+function getRandomIntInclusive(min, max) {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+}
 
 function range(int) {
   const arr = [];
@@ -8,18 +12,18 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
-  if (a[key] < b[key]) {
+function sortByKey(org, compare, key) {
+  if (org[key] < compare[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  } if (org[key] > compare[key]) {
     return 1;
   }
   return 0;
 }
 
 document.body.addEventListener('submit', async (e) => {
-  e.preventDefault(); // this stops whatever the browser wanted to do itself.
-  const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
+  e.preventDefault(); 
+  const form = $(e.target).serializeArray(); 
   fetch('/api', {
     method: 'POST',
     headers: {
@@ -29,8 +33,27 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+      console.log(fromServer)
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const newArr = range(10);
+      const newArr2 = newArr.map(() => {
+        const number = getRandomIntInclusive(0, 243);
+        return fromServer[number];
+      });
+
+      const reverseList = newArr2.sort((org, compare) => sortByKey(org, compare, 'name'));
+      const ul = document.createElement('ol');
+      ul.className = 'flex-inner';
+      $('form').prepend(ul);
+
+      reverseList.forEach((el, i) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        $(ul).append(li);
+      });
     })
     .catch((err) => console.log(err));
 });
